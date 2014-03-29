fac1f44738db6f97335bfbf6752e757ffb240a11
Alex Walker <a.mlw.walker@gmail.com>
2014-03-29 19:46:59 +0000

    Creating Species object. Now all actions are performed by that

--- src/com/walker/evolution/Main.java
+++ src/com/walker/evolution/Main.java
@@ -11,62 +11,32 @@ public final class Main {
 	 */
 	public static void main(String[] args) {
 
-		/*
-		 * To create a creature, first you must define its perfect form. Once
-		 * you have a perfect creature type, you can generate a creature. Its
-		 * "perfect form" defines its type. The evolution of a creature will
-		 * always head for that perfection.
-		 */
-		List<Creature> listOfParents = new ArrayList<Creature>();
-		List<Creature> listOfChildren = new ArrayList<Creature>();
-
-		listOfParents = God.getInstance().performMiracle(
-				"f6e622e2ff23483d8861445945d7d987");
 		int bestCreature = 0;
-		for (int i = 0; i < 20; i++) { // do for 5 generations
+		List<Species> listOfSpecies = new ArrayList<Species>();
 
-			for (Creature creature : listOfParents) {
+		Species alex = new Species("f6e622e2ff23483d8861445945d7d987");
+		listOfSpecies.add(alex);
+		// An amount of Generations to calculate
+		for (Species species : listOfSpecies) {
 
-				God.getInstance().evolveCreature(creature, 3);
+			for (int i = 0; i < 20; i++) {
 
-//				System.out.println("Genome: " + creature.getGenome()
-//						+ "\nChildG: "
-//						+ creature.getChildren().get(0).getGenome()
-//						+ " Perfection: "
-//						+ God.getInstance().calculatePerfection(creature));
-				creature.setPerfectness(God.getInstance().calculatePerfection(
-						creature));
-				God.getInstance().killWorstChild(creature);
-				listOfChildren.addAll(creature.getChildren());
-			}
+				species.copulate(3);
+				bestCreature = species.bestChild();
+				species.prepForNextGen();
 
-			System.out.println(listOfParents.size() + " : "
-					+ listOfChildren.size());
-
-			// All parents die. Children become parents.
-			listOfParents.clear();
-			listOfParents.addAll(listOfChildren);
-			listOfChildren.clear();
-			System.out.println(listOfParents.size() + " : "
-					+ listOfChildren.size());
-			
-			int minimum = listOfParents.get(0).getPerfectness();
-			for (Creature creature : listOfParents) {
-				System.out.println(creature.getPerfectness());
-					if (creature.getPerfectness() < minimum){
-						minimum = creature.getPerfectness();
-					}
-			}
-			bestCreature = minimum;
-			if (bestCreature < 300){ //break if a creature is within 400 of the perfect genome
-				System.out.println("breaking:\n");
-				System.out.println("Generation: " + i + " Number of Creatures: " + listOfParents.size() + " best Genome: " + bestCreature);
-				break;
+				if (bestCreature < 300) { // break if a creature is within 400
+											// of the perfect genome
+					System.out.println("breaking:\n");
+					System.out.println("Generation: " + i
+							+ " Number of Creatures: "
+							+ species.getPopulation().size() + " best Genome: "
+							+ bestCreature);
+					break;
+				}
 			}
 		}
 
-		// now we have a new creature
-		// one option is to just kill the worst creature.
 	}
 
 }
---
+++ src/com/walker/evolution/Species.java
@@ -0,0 +1,72 @@
+package com.walker.evolution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Species {
+
+	private String perfectGenome;
+	/*
+	 * We will always have children and parents. Except the first time
+	 * when God performs a miracle.
+	 */
+	private List<Creature> listOfParents = new ArrayList<Creature>();
+	private List<Creature> listOfChildren = new ArrayList<Creature>();
+	
+	public Species(String perfectGenome){
+		this.perfectGenome = perfectGenome;
+		
+		/*
+		 * God is performing a miracle. A seed genome is fed in
+		 * as the "perfect Genome"
+		 */
+		listOfParents = God.getInstance().performMiracle(
+				perfectGenome);
+		
+	}
+	public void copulate(int childCount){
+		for (Creature creature : listOfParents) {
+			//For each creature, create a number of children
+			//in the process of "evolution"
+			God.getInstance().evolveCreature(creature, childCount);
+			
+			//Let a creature know its perfectness so that God can ask for it later. How humiliating
+//			creature.setPerfectness(God.getInstance().calculatePerfection(
+//					creature));
+			//Kill a parents worst quality child - its weak
+			God.getInstance().killWorstChild(creature);
+			//Add all the children to the list of all children of this creature type
+			listOfChildren.addAll(creature.getChildren());
+		}
+		//how many parents : children
+	}
+	
+	public int bestChild(){
+		//Find the best child of the generation
+		int minimum = listOfChildren.get(0).getPerfectness();
+		for (Creature creature : listOfChildren) {
+			System.out.println(creature.getPerfectness());
+				if (creature.getPerfectness() < minimum){
+					minimum = creature.getPerfectness();
+				}
+		}
+		return minimum;
+	}
+	
+	public void prepForNextGen(){
+		// All parents die. Children become parents.
+		listOfParents.clear();
+		listOfParents.addAll(listOfChildren);
+		//Clear the children as they are now ready to be the next generations parents
+		listOfChildren.clear();
+	}
+	public String getPerfectGenome() {
+		return perfectGenome;
+	}
+	public void setPerfectGenome(String perfectGenome) {
+		this.perfectGenome = perfectGenome;
+	}
+	public List<Creature> getPopulation() {
+		return listOfParents;
+	}
+}
